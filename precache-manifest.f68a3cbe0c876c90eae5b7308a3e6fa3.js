db3bb53bce7ab0c7df04b2765ea3e0f846f78542
Gopi Patel <ask2gopi@gmail.com>
2019-10-07 19:11:54 +1100

    Updates

---
+++ precache-manifest.f68a3cbe0c876c90eae5b7308a3e6fa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f532b6e2620902958e1a9db6636f9385",
+    "url": "/whats-the-weather/index.html"
+  },
+  {
+    "revision": "9be13277ee594435c75b",
+    "url": "/whats-the-weather/static/css/main.fc66f51a.chunk.css"
+  },
+  {
+    "revision": "03b382700008594e94e8",
+    "url": "/whats-the-weather/static/js/2.190cac99.chunk.js"
+  },
+  {
+    "revision": "9be13277ee594435c75b",
+    "url": "/whats-the-weather/static/js/main.a77667a6.chunk.js"
+  },
+  {
+    "revision": "8385f2dc26b153ed01ab",
+    "url": "/whats-the-weather/static/js/runtime-main.6ef66606.js"
+  }
+]);
