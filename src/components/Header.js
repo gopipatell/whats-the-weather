8a0324b8585c4b56671b33a00d402cea7a215551
Gopi Patel <ask2gopi@gmail.com>
2019-10-07 12:18:33 +1100

    header component added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1> Weather App </h1>
+      <h3> What's the weather today?</h3>
+    </div>
+  );
+}
+
+export default Header;
